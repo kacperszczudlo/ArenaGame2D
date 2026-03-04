cbfd842e929872ab850f74110487a389c9bf4fe5
Kacper Szczudło <kacper.szczudlo@gmail.com>
2026-03-04 20:38:36 +0100

    feat: implement player movement and setup basic animations

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour 
+{
+    [SerializeField] private float m_speed = 4.0f;
+
+    private Animator m_animator;
+    private Rigidbody2D m_body2d;
+    private float m_delayToIdle = 0.0f;
+
+    void Start()
+    {
+        m_animator = GetComponent<Animator>();
+        m_body2d = GetComponent<Rigidbody2D>();
+
+        m_body2d.gravityScale = 0f;
+        m_animator.SetBool("Grounded", true);
+    }
+
+    void Update()
+    {
+        float inputX = Input.GetAxis("Horizontal");
+        float inputY = Input.GetAxis("Vertical");
+
+        if (inputX > 0)
+            GetComponent<SpriteRenderer>().flipX = false;
+        else if (inputX < 0)
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        m_body2d.linearVelocity = new Vector2(inputX * m_speed, inputY * m_speed);
+
+        if (Mathf.Abs(inputX) > Mathf.Epsilon || Mathf.Abs(inputY) > Mathf.Epsilon)
+        {
+            m_delayToIdle = 0.05f;
+            m_animator.SetInteger("AnimState", 1);
+        }
+        else
+        {
+            m_delayToIdle -= Time.deltaTime;
+            if(m_delayToIdle < 0)
+                m_animator.SetInteger("AnimState", 0);
+        }
+    }
+}
